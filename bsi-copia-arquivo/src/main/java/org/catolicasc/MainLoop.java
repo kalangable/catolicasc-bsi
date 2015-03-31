cc22c14b760ab0c8b7d6323dca416210e527413e
Adilson Krasovski <kalangable@gmail.com>
2015-03-31 01:52:40 -0300

    Correção dos modelo de acesso ao Resource

--- bsi-copia-arquivo/src/main/java/org/catolicasc/MainLoop.java
+++ bsi-copia-arquivo/src/main/java/org/catolicasc/MainLoop.java
@@ -2,6 +2,7 @@ package org.catolicasc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,13 +16,17 @@ public class MainLoop {
 
 		ExecutorService service = Executors.newFixedThreadPool( 3 );
 
-		List<Future<Long>> future = new ArrayList<Future<Long>>();
+		List<Future<Long>> listFuture = new ArrayList<Future<Long>>();
 
-		future.add( service.submit( new CopiaCronometrada( "Lorem3.txt", "C:\\tmp\\Lorem3.txt" ) ) );
-		future.add( service.submit( new CopiaCronometrada( "Lorem1.txt", "C:\\tmp\\Lorem1.txt" ) ) );
-		future.add( service.submit( new CopiaCronometrada( "Lorem2.txt", "C:\\tmp\\Lorem2.txt" ) ) );
+		Callable<Long> cc1 = new CopiaCronometrada( "Lorem3.txt", "C:\\tmp\\Lorem3.txt" );
+		Future fut = service.submit( cc1 );
+		listFuture.add( fut );
+		// future.add( service.submit( new CopiaCronometrada( "Lorem1.txt",
+		// "C:\\tmp\\Lorem1.txt" ) ) );
+		// future.add( service.submit( new CopiaCronometrada( "Lorem2.txt",
+		// "C:\\tmp\\Lorem2.txt" ) ) );
 
-		for ( Future<Long> future2 : future ) {
+		for ( Future<Long> future2 : listFuture ) {
 			System.out.println( "Tempo para a execução da copia = " + future2.get() );
 		}
 
